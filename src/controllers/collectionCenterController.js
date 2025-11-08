dc6943d5309a5b04088cd998a4cb2686f7c1ca7b
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-11-08 13:15:33 -0600

    Fix

--- src/controllers/collectionCenterController.js
+++ src/controllers/collectionCenterController.js
@@ -5,8 +5,12 @@ exports.getCollectionCenters = async (req, res) => {
     try {
         const centers = await CollectionCenter.find();
         res.json(centers);
+        //#swagger.responses[200] = { description: "Lista de centros de acopio obtenida exitosamente." }
+        return res.status(200).send(centers);
     } catch (error) {
         res.status(500).json({ message: error.message });
+        //#swagger.responses[500] = { description: "Error al obtener la lista de centros de acopio." }
+        return res.status(500).send(false);
     }
 };
 
@@ -15,22 +19,33 @@ exports.getCollectionCenter = async (req, res) => {
     try {
         const center = await CollectionCenter.findById(req.params.id);
         if (center) {
+            //#swagger.responses[200] = { description: "Centro de acopio obtenido exitosamente." }
             res.json(center);
+            return res.status(200).send(center);
         } else {
+            //#swagger.responses[404] = { description: "Centro de acopio no encontrado." }
             res.status(404).json({ message: 'Centro de acopio no encontrado.' });
+            return res.status(404).send(false);
         }
     } catch (error) {
+            //#swagger.responses[500] = { description: "Error al obtener el centro de acopio." }
         res.status(500).json({ message: error.message });
+        return res.status(500).send(false);
     }
 };
 
 // Get centers by manager
 exports.getCentersByManager = async (req, res) => {
     try {
+
         const centers = await CollectionCenter.find({ idGestor: req.params.managerId });
+        // #swagger.responses[200] = { description: "Centros de acopio obtenidos exitosamente para el gestor especificado." }
         res.json(centers);
+        return res.status(200).send(centers);
     } catch (error) {
+        // #swagger.responses[500] = { description: "Error al obtener los centros de acopio para el gestor especificado." }
         res.status(500).json({ message: error.message });
+        return res.status(500).send(false);
     }
 };
 
@@ -43,9 +58,13 @@ exports.createCollectionCenter = async (req, res) => {
 
     try {
         const newCenter = await center.save();
+        // #swagger.responses[201] = { description: "Centro de acopio creado exitosamente." }
         res.status(201).json(newCenter);
+        return res.status(201).send(newCenter);
     } catch (error) {
+        // #swagger.responses[400] = { description: "Error al crear el centro de acopio." }
         res.status(400).json({ message: error.message });
+        return res.status(400).send(false);
     }
 };
 
@@ -58,12 +77,18 @@ exports.updateCollectionCenter = async (req, res) => {
             center.nombre = req.body.nombre || center.nombre;
 
             const updatedCenter = await center.save();
+            // #swagger.responses[200] = { description: "Centro de acopio actualizado exitosamente." }
             res.json(updatedCenter);
+            return res.status(200).send(updatedCenter);
         } else {
+            // #swagger.responses[404] = { description: "Centro de acopio no encontrado." }
             res.status(404).json({ message: 'Centro de acopio no encontrado.' });
+            return res.status(404).send(false);
         }
     } catch (error) {
+        // #swagger.responses[400] = { description: "Error al actualizar el centro de acopio." }
         res.status(400).json({ message: error.message });
+        return res.status(400).send(false);
     }
 };
 
@@ -73,11 +98,16 @@ exports.deleteCollectionCenter = async (req, res) => {
         const center = await CollectionCenter.findById(req.params.id);
         if (center) {
             await center.remove();
+            // #swagger.responses[200] = { description: "Centro de acopio eliminado exitosamente." }
             res.json({ message: 'Centro de acopio eliminado exitosamente.' });
+
         } else {
+            // #swagger.responses[404] = { description: "Centro de acopio no encontrado." }
             res.status(404).json({ message: 'Centor de acopio no encontrado.' });
+
         }
     } catch (error) {
+        // #swagger.responses[500] = { description: "Error al eliminar el centro de acopio." }
         res.status(500).json({ message: error.message });
     }
 };
--- swagger.js
+++ swagger.js
@@ -1,11 +1,12 @@
 const swaggerAutogen = require("swagger-autogen")();
+const isProduction = process.env.NODE_ENV === "production";
 
 const doc = {
   info: {
     title: "API de Navegación SGAR",
     description: "Documentación de la API para coordenadas y ubicaciones geográficas."},
-  host: "https://sgar-navigation.vercel.app",
-  schemes: ["http"],
+  host: isProduction ? "https://sgar-navigation.vercel.app" : "localhost:3000",
+  schemes: [ isProduction ? "https" : "http" ],
 };
 
 const outputFile = "./swagger_output.json";
