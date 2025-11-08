5bf86bc5ef9a09c7e413f45dc23e11a29dfa70fd
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-11-08 13:27:12 -0600

    redo

--- vercel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-    # Command 1
-    npm install
-    # Command 2 (only runs if npm install succeeds)
-    node ./swagger.js
-    # Command 3 (only runs if the previous commands succeed)
-    npm run build
