ce53bb8ebd661271e11d2b4a497d64274c686206
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-11-25 20:09:49 -0600

    more fixes

--- src/models/CollectionLocation.js
+++ src/models/CollectionLocation.js
@@ -1,31 +1,35 @@
 const mongoose = require('mongoose');
 
-const collectionLocationSchema = new mongoose.Schema({
+const collectionLocationSchema = new mongoose.Schema(
+  {
     idCenter: {
-        type: String,
-        required: true
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CollectionCenter",
+      required: true,
     },
     latitud: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     longitud: {
-        type: Number,
-        required: true
+      type: Number,
+      required: true,
     },
     descripcion: {
-        type: String
+      type: String,
     },
     horaApertura: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     horaCierre: {
-        type: String,
-        required: true
-    }
-}, {
-    timestamps: true
-});
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model('CollectionLocation', collectionLocationSchema);
